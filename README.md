7043c823925b743802a36dd80c12c88dbbc2bab9
claudiobmontenegro <39444167+claudiobmontenegro@users.noreply.github.com>
2019-04-04 19:39:48 -0300

    Subindo arquivos

---
+++ README.md
@@ -0,0 +1,6 @@
+# rpg-the-chosen
+Jogo de RPG based on turns, para a disciplina de Introdução a Linguagem de Programação em C como proposta de avaliação do terceiro estágio.
+
+# contributors
+- [Claudio Bessa Montenegro](http://github.com/claudiobmontenegro)
+- [Gabriel de Carvalho Andrade](http://github.com/gabrielcandrade)
---
+++ personagem.h
@@ -0,0 +1,34 @@
+// Dados dos personagens
+
+struct Personagem{
+	int nivel;
+	int exp;
+	int hp;
+	int moedas;
+	int pontKills;
+};
+
+struct Atributos{
+	int forca;
+	int destreza;
+	int inteligencia;
+	int sorte;
+};
+
+struct Arma{
+	int arco;
+	int espada;
+	int varinha;
+};
+
+struct Pet{
+	int tigre;
+	int vibora;
+	int aguia;
+};
+
+struct Montaria{
+	int buffalo;
+	int cavalo;
+	int dragao;
+};
